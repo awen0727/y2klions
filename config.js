bfaccb7bdc710d9e4cf4e393e8b3941c79a8bbc7
Chris Wang <chriswang@ChrisdeMacBook-Pro.local>
2026-06-30 01:12:00 +0800

    Update production Apps Script URL

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   "use strict";
 
   window.Y2kConfig = {
-    apiUrl: "https://script.google.com/macros/s/AKfycbwGno3DV52zowikkhUk9J985N5GP2ANzeccTuEmETAZFBnzgx-iKw3Kqsd3vMD3YcyjTQ/exec",
+    apiUrl: "https://script.google.com/macros/s/AKfycbyEA2gy9_OP034QRm-8gHiY-HRLu_VP0KHluPWCx_FElWRP0-9JxbfZIHgx9hLht71h1w/exec",
     liffId: "2010549563-FKKbO0xy",
     displayToken: "y2k-display-2026",
     systemName: "千禧獅子會",

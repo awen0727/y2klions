bb2fc22d1c20a96a1b5c0f12eb74a372d8accd23
Chris Wang <chriswang@ChrisdeMacBook-Pro.local>
2026-07-01 22:02:58 +0800

    Update production API to v7 deployment

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   "use strict";
 
   window.Y2kConfig = {
-    apiUrl: "https://script.google.com/macros/s/AKfycbwALS6nTKiAOnTCQqgtW2FQlAkC0-SXcC-WCMxt20SsvIvK_kFzHLSsIMbi9Lvdp4GfpA/exec",
+    apiUrl: "https://script.google.com/macros/s/AKfycbyIxyq_txaf0mpnDteFa3lKV5IXKRL1MgGHrkEtDDbLcWEzVgzt9szsYu-fKN2DOI3Mnw/exec",
     liffId: "2010549563-FKKbO0xy",
     displayToken: "y2k-display-2026",
     systemName: "千禧獅子會",
